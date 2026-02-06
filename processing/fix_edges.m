575709e6f06b8773bf9a2c90fee893f29a0b15b2
bfalecki <01158532@pw.edu.pl>
2026-02-06 14:25:14 +0100

    fix in fix_edges

--- processing/fix_edges.m
+++ processing/fix_edges.m
@@ -11,7 +11,12 @@ function signal_fixed = fix_edges(signal,start_samples, end_samples, depth)
 
 signal_fixed = signal;
 for k = 1:length(start_samples)
-    samples_to_avg_start = start_samples(k):start_samples(k)+depth;
+    %  start_samples(k)+1 as we don't want to use the first sample
+    % it is outstanding. The issue is connected with compl_diff function as
+    % it adds one sample to the beggining, not to the end of signal.
+    % In the second case, we would rather use end_samples(k)-1 instead of end_samples(k)
+    % and start_samples(k) would be OK
+    samples_to_avg_start = start_samples(k)+1:start_samples(k)+depth;
     signal_fixed(start_samples(k)) = median(signal(samples_to_avg_start));
     samples_to_avg_end = end_samples(k) - depth:end_samples(k);
     signal_fixed(end_samples(k)) = median(signal(samples_to_avg_end));
